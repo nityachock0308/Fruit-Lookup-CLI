169cc386c69b678373d6ecef1802216ed15659b6
nityachock0308 <nityachock@gmail.com>
2025-03-05 18:18:22 +0000

    Update Fruit API Test 1.py

--- Fruit API Test 1.py
+++ Fruit API Test 1.py
@@ -9,6 +9,7 @@ def get_fruit_info(fruit_name):
     # Print the response to debug
     print(response.status_code)  # Check if request was successful (should be 200)
     print(response.json())  # Print the full response data
+
     return response.json()
 
 #get information about an apple
@@ -16,8 +17,8 @@ apple_info = get_fruit_info("apple")
 
 print(apple_info["name"]) #Ouput: "Apple"
 
-# Check if 'nutrients' exists before accessing it
-if "nutrients" in apple_info:
+# Check if 'nutritions' exists before accessing it
+if "nutritions" in apple_info:
     print(apple_info["nutritions"]["carbohydrates"])  # Output: carbohydrate value
 else:
-    print("Key 'nutrients' not found in API response.")
+    print("Key 'nutritions' not found in API response.")
